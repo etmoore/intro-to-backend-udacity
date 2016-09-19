6cc0ad5ffeb92254105d515d586f90575a76c363
Evan Moore <etmoore@gmail.com>
2016-09-18 18:29:33 -0600

    remove TestHandler

--- hello_world/main.py
+++ hello_world/main.py
@@ -2,30 +2,23 @@ import webapp2
 
 
 form = """
-<form method="post" action="/testform">
-    <input name="q">
+<form method="post">
+    What is your birthday?
+
+    <br>
+    <input type="text" name="month" placeholder="month">
+    <input type="text" name="day" placeholder="day">
+    <input type="text" name="year" placeholder="year">
+
+    <br>
+    <br>
     <input type="submit">
 </form>
 """
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/html'
         self.response.write(form)
 
-class TestHandler(webapp2.RequestHandler):
-    def post(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(self.request)
-
-    def get(self):
-        # q = self.request.get("q")
-        # self.response.write(q)
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(self.request)
-
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/testform', TestHandler),
-], debug=True)
+app = webapp2.WSGIApplication([ ('/', MainPage), ], debug=True)
 

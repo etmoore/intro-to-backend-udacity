6fcd8a59f921e9cceaed3a7d17aacb1df4f1bce1
Evan Moore <etmoore@gmail.com>
2016-10-22 23:20:23 -0600

    redirect to /thanks route when date entered correctly

--- hello_world/main.py
+++ hello_world/main.py
@@ -74,7 +74,11 @@ class MainPage(webapp2.RequestHandler):
             self.write_form("That doesn't look right to me, friend.",
                             user_month, user_day, user_year)
         else:
-            self.response.out.write("Thanks! That's a totally valid day!")
+            self.redirect("/thanks");
 
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write("Thanks! That's a totally valid day!")
 
-app = webapp2.WSGIApplication([ ('/', MainPage), ], debug=True)
+app = webapp2.WSGIApplication([ ('/', MainPage), ('/thanks', ThanksHandler)],
+                              debug=True)

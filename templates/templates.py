5146666b688f07fed5b8de6cdc34e4ad8034f18e
Evan Moore <etmoore@gmail.com>
2016-10-26 22:52:24 -0600

    and render functions

--- templates/templates.py
+++ templates/templates.py
@@ -36,6 +36,13 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, templated, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
 class MainPage(Handler):
     def get(self):
         output = form_html

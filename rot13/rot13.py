708efab02b7cb7eba13d44c762ded7431ed1946b
Evan Moore <etmoore@gmail.com>
2016-10-29 12:45:25 -0600

    form posts message

---
+++ rot13/rot13.py
@@ -0,0 +1,32 @@
+import os
+import jinja2
+import webapp2
+
+# configuration for jinja
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    """Renders via jinja2 template engine"""
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
+    def get(self):
+        self.render("rot13_form.html")
+
+    def post(self):
+        message = self.request.get("message")
+        print(message)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ], debug=True)

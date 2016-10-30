2b272c510d1bdd465736c7dd7f35434404c04015
Evan Moore <etmoore@gmail.com>
2016-10-30 16:24:32 -0600

    validate un, pw, email, and display welcome page

--- user-signup/user-signup.py
+++ user-signup/user-signup.py
@@ -1,4 +1,5 @@
 import os
+import re
 import jinja2
 import webapp2
 
@@ -6,6 +7,18 @@ import webapp2
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return USER_RE.match(username)
+
+PASSWORD_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return PASSWORD_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+def valid_email(email):
+    return EMAIL_RE.match(email)
 
 class Handler(webapp2.RequestHandler):
     """Renders via jinja2 template engine"""
@@ -19,16 +32,37 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class MainPage(Handler):
+class SignupPage(Handler):
     def get(self):
         self.render("user_signup_form.html")
 
     def post(self):
-        text = self.request.get("text")
-        cipher = text.encode("rot13")
-        self.render("user_signup_form.html", cipher = cipher)
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
 
+        if (valid_username(username) and
+            valid_password(password) and
+            password == verify and
+            valid_email(email)):
+            self.render("welcome.html", username = username)
+        else:
+            error = {}
+            if not valid_username(username):
+                error["username"] = "That's not a valid username"
+            if not valid_password(password):
+                error["password"] = "That's not a valid password"
+            if not password == verify:
+                error["verify"] = "Your passwords didn't match"
+            if not valid_email(email):
+                error["email"] = "That's not a valid email"
+
+            self.render("user_signup_form.html",
+                        username = username,
+                        email = email,
+                        error = error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', SignupPage),
     ], debug=True)

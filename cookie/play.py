6e96af9b28fb0d0528d8849a4f736aebedfcdf84
Evan Moore <etmoore@gmail.com>
2016-12-19 21:22:32 -0700

    set cookie for number of visits

--- cookie/play.py
+++ cookie/play.py
@@ -22,6 +22,18 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.write('test!')
+        self.response.headers['Content-Type'] = 'text/plain'
+        visits = self.request.cookies.get('visits', '0')
+        if visits.isdigit():
+            visits = int(visits) + 1
+        else:
+            visits = 0
+
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+
+        if visits > 10:
+            self.write("You are the best ever!")
+        else:
+            self.write("You've been here %s times!" % visits)
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)

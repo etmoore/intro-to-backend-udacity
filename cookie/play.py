1b8d4831b90cdc48c92ded2839dadea681b48cf1
Evan Moore <etmoore@gmail.com>
2016-12-26 17:32:45 -0500

    secure cookie

--- cookie/play.py
+++ cookie/play.py
@@ -1,12 +1,25 @@
 import os
 import webapp2
 import jinja2
+import hashlib
+import pdb
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
+
+def hash_str(s):
+    return hashlib.md5(s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
@@ -23,15 +36,20 @@ class Handler(webapp2.RequestHandler):
 class MainPage(Handler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        visits = self.request.cookies.get('visits', '0')
-        if visits.isdigit():
-            visits = int(visits) + 1
-        else:
-            visits = 0
+        visits = 0
+        visit_cookie_str = self.request.cookies.get('visits')
+        if visit_cookie_str:
+            cookie_val = check_secure_val(visit_cookie_str)
+            if cookie_val:
+                visits = int(cookie_val)
+                print visits
 
-        self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+        visits += 1
+        new_cookie_val = make_secure_val(str(visits))
 
-        if visits > 10:
+        self.response.headers.add_header('Set-Cookie', 'visits=%s' % new_cookie_val)
+
+        if visits > 10000:
             self.write("You are the best ever!")
         else:
             self.write("You've been here %s times!" % visits)

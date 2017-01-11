9a86277ba4735fb33128a26c895e8921a5f7edca
Evan Moore <etmoore@gmail.com>
2017-01-10 20:21:48 -0700

    display username through cookie

--- user-registration/registration.py
+++ user-registration/registration.py
@@ -1,6 +1,7 @@
 import os
 import webapp2
 import jinja2
+import re
 
 from google.appengine.ext import db
 
@@ -23,4 +24,54 @@ class SignupPage(Handler):
     def get(self):
         self.render("user-signup-form.html")
 
-app = webapp2.WSGIApplication([('/', SignupPage)], debug=True)
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+        def valid_username(username):
+            USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+            return USER_RE.match(username)
+
+        def valid_password(password):
+            PASSWORD_RE = re.compile(r"^.{3,20}$")
+            return PASSWORD_RE.match(password)
+
+        def valid_email(email):
+            EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+            return EMAIL_RE.match(email)
+
+        if (valid_username(username) and
+            valid_password(password) and
+            password == verify and
+            valid_email(email)):
+
+            self.response.set_cookie('username', username)
+            self.redirect("/welcome")
+
+        else:
+            error = {}
+            if not valid_username(username):
+                error["username"] = "That's not a valid username"
+            if not valid_password(password):
+                error["password"] = "That's not a valid password"
+            if not password == verify:
+                error["verify"] = "Your passwords didn't match"
+            if not valid_email(email):
+                error["email"] = "That's not a valid email"
+
+            self.render("user_signup_form.html",
+                        username=username,
+                        email=email,
+                        error=error)
+
+class WelcomePage(Handler):
+    def get(self):
+        username = self.request.cookies.get('username')
+        self.render("welcome.html", username=username)
+
+
+app = webapp2.WSGIApplication([('/signup', SignupPage),
+                               ('/welcome', WelcomePage)]
+                               , debug=True)
